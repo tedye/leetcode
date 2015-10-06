2560c3451ed1d8cd93470f876b13140df1f1f40d
tedye <tedye@bu.edu>
2015-10-06 10:42:05 -0700

    leetcode_021
    linked list travesal

---
+++ Python/leetcode.021.Merge-Two-Sorted-Lists.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        temp = dummy
+        while l1 or l2:
+            if l1 and l2:
+                if l1.val < l2.val:
+                    temp.next = ListNode(l1.val)
+                    l1 = l1.next
+                else:
+                    temp.next = ListNode(l2.val)
+                    l2 = l2.next
+            elif l1:
+                temp.next = ListNode(l1.val)
+                l1 = l1.next
+            elif l2:
+                temp.next = ListNode(l2.val)
+                l2 = l2.next
+            temp = temp.next
+        return dummy.next
