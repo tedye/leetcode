6324c44ef9436f383b677a0173f93748e80b1d6b
tedye <tedye@bu.edu>
2015-10-06 11:10:50 -0700

    leetcode_034
    binary search variation

---
+++ Python/leetcode.034.search-for-a-range.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums: return [-1,-1]
+        left = self.findfloor(nums, target)
+        right = self.findceil(nums, target)
+        try:
+            if nums[left] == target and nums[right] == target:
+                return [left, right]
+        except:
+            pass
+        return [-1,-1]
+        
+    def findfloor(self,nums,target):
+        i = 0
+        l = len(nums)-1
+        while i <= l:
+            mid = i + (l-i) // 2            
+            if nums[mid] < target:
+                i = mid + 1 
+            else:
+                l = mid - 1
+        return i
+        
+    def findceil(self,nums, target):
+        i = 0
+        l = len(nums)-1
+        while i <= l:
+            mid = i + (l-i) // 2            
+            if nums[mid] > target:
+                l = mid - 1 
+            else:
+                i = mid + 1
+        return l
