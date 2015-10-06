8199cc1c826d66b4c5569ff8e0f14fea2e754dd2
tedye <tedye@bu.edu>
2015-10-06 10:28:19 -0700

    leetcode_018
    same logic with 3 sum problem

---
+++ Python/leetcode.018.4Sum.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        if len(nums) < 4:
+            return []
+        res = set()
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                t = target -nums[i] - nums[j]
+                s = j + 1
+                e = len(nums) - 1
+                while s < e:
+                    x = nums[s] +  nums[e]
+                    if x == t:
+                        res.add(tuple([nums[i],nums[j], nums[s], nums[e]]))
+                    if x < t:
+                        s += 1
+                    else:
+                        e -= 1
+        return [list(r) for r in res]
+        
