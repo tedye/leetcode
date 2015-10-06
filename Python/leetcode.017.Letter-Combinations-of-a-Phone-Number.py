354e066f045e3f614e2e46571a2ff96b0a88b662
tedye <tedye@bu.edu>
2015-10-06 10:27:00 -0700

    leetcode_017
    recursion

---
+++ Python/leetcode.017.Letter-Combinations-of-a-Phone-Number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        d = {'2':'abc', '3':'def','4':'ghi', '5':'jkl', '6':'mno', '7': 'pqrs', '8':'tuv', '9':'wxyz'}
+        
+        cur = []
+        res = []
+        self.helper(digits, res, cur, d)
+        return res
+        
+    def helper(self, digits, res,cur,d):
+        if not digits:
+            res.append(''.join(cur))
+            return
+        
+        c = digits[0]
+        for j in d[c]:
+            cur.append(j)
+            self.helper(digits[1:],res,cur,d)
+            cur.pop(-1)
