99d758063651e7609de9b93e91e8b2fc20da4c26
tedye <tedye@bu.edu>
2015-10-06 10:23:31 -0700

    leetcode_016
    similar to 015 3sum problem

---
+++ Python/leetcode.016.3Sum-Closest.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        l = len(nums)
+        threshold = 0x7fffffff
+        s = 0
+        for i in range(l):
+            j = i + 1
+            k = l - 1
+            while j < k:
+                sum0 =  nums[i] + nums[j] + nums[k]
+                diff = abs(target - sum0)
+                if diff == 0: return sum0
+                
+                if diff < threshold:
+                    threshold = diff
+                    s = sum0
+                
+                if sum0 < target:
+                    j += 1
+                else:
+                    k -= 1
+                    
+        return s
