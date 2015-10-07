175f5daf014290aadf61b8604869fda2212ea375
tedye <tedye@bu.edu>
2015-10-07 09:56:40 -0700

    leetcode_071
    stack

---
+++ Python/leetcode.071.simplify-path.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        p = path.split('/')
+        stack = []
+        for token in p:
+            if not token or token == '.' or token == '..' and not stack:
+                continue
+            elif token == '..':
+                stack.pop(-1)
+            else:
+                stack.append(token)
+        return '/'+'/'.join(stack)
