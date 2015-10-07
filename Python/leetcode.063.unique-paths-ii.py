e288bf12e48fb4ab6f59ea80fbe21da17bf2bca5
tedye <tedye@bu.edu>
2015-10-07 09:42:55 -0700

    leetcode_063
    DP

---
+++ Python/leetcode.063.unique-paths-ii.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        
+        dp = [0] * (n+1)
+        dp[0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[j+1] = 0
+                else:
+                    dp[j+1] += dp[j]
+            dp[0] = 0
+        return dp[-1]
